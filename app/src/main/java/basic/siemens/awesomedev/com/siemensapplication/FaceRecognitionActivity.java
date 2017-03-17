c4de6765939a368d062c406a9a876a09265d39b1
Anirudh <anirudhaggarwal2@gmail.com>
2017-03-18 03:52:16 +0530

    minor fix

--- app/src/main/java/basic/siemens/awesomedev/com/siemensapplication/FaceRecognitionActivity.java
+++ app/src/main/java/basic/siemens/awesomedev/com/siemensapplication/FaceRecognitionActivity.java
@@ -41,6 +41,7 @@ public class FaceRecognitionActivity extends AppCompatActivity implements View.O
 
             Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
             intent.setType("image/*");
+            intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
             Intent pickIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
             pickIntent.setType("image/*");
             Intent chooser = Intent.createChooser(intent,"Select at max 6 images..");
